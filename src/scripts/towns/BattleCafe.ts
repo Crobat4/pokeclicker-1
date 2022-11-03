b20efaec2aed0a9434b9b3f371ac1ef6061ed587
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2022-11-03 19:55:13 -0300

    Revert "Battle Cafe spins changes"
    This reverts commit ee91d0317092ed073de332602db4cac5d55eb9e0.

--- src/scripts/towns/BattleCafe.ts
+++ src/scripts/towns/BattleCafe.ts
@@ -44,22 +44,11 @@ class BattleCafeController {
     static isSpinning = ko.observable<boolean>(false);
     static clockwise = ko.observable<boolean>(false);
 
-    static calcMaxSpins() : number {
-        // Give additional max spins for each sweet type completed
+    static spinsPerDay() : number {
+        // Give additional spins for each sweet type completed
         return this.baseMaxSpins + GameHelper.enumStrings(GameConstants.AlcremieSweet)
             .filter((s) => BattleCafeController.getCaughtStatus(GameConstants.AlcremieSweet[s])() >= CaughtStatus.Caught)
             .length;
-    }
-
-    static checkAllSweetsCompleted() {
-        const totalSweets = GameHelper.enumStrings(GameConstants.AlcremieSweet).length;
-        const currentCompletedSweets = GameHelper.enumStrings(GameConstants.AlcremieSweet)
-        .filter((s) => BattleCafeController.getCaughtStatus(GameConstants.AlcremieSweet[s])() >= CaughtStatus.Caught)
-        .length;
-        if (currentCompletedSweets == totalSweets) {
-            return true;
-        }
-        return false;
     }
 
     public static spin(clockwise: boolean) {
@@ -118,17 +107,15 @@ class BattleCafeController {
     /* *
      * The period for spin recharges (in hours)
      */
-    // public static period = 2;
+    public static period = 2;
 
     /**
      * Recharges spin
      */
     public static rechargeSpin(hour: number): void {
-        // Recharge a spin every 2 hours (1 if all sweets are completed)
-        const period = BattleCafeController.checkAllSweetsCompleted() ? 1 : 2;
-        if ((hour % period) == 0) {
+        if ((hour % BattleCafeController.period) == 0) {
             const recharge = BattleCafeController.spinsLeft() + BattleCafeController.defaultRecharge;
-            BattleCafeController.spinsLeft(Math.min(BattleCafeController.calcMaxSpins(), recharge));
+            BattleCafeController.spinsLeft(Math.min(BattleCafeController.baseMaxSpins, recharge));
         }
     }
 
